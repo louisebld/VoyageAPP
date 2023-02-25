4b563b12d7332876f4cbbc89923e23216ccd8814
Louise <Louiisebld@gmail.com>
2023-02-25 12:53:57 +0100

    add css and image

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ const handleTempsChargementChange = (event) => {
 
   const sendRequest = () => {
           // console.log("bjr");
-
     soap.createClient('http://127.0.0.1:8080/distance?wsdl', function (err, client) {
       client.distance({ distance: distance, autonomie: autonomie, tempschargement: tempsChargement }, function (err, result) {
         console.log(result);
@@ -40,20 +39,26 @@ const handleTempsChargementChange = (event) => {
     <>
       <div className="App">
         <h1 className="title">Voyage APP</h1>
+        <div class="container">
         <div className="divform">
         <form>
-          <label>Distance</label>
-          <input type="number" name="distance" value={distance} onChange={handleDistanceChange}/>
+          <label>Distance </label>
+          <input type="number" name="distance" value={distance} onChange={handleDistanceChange} placeholder="km"/>
           <label>Autonomie</label>
-          <input type="number" name="autonomie" value={autonomie} onChange={handleAutonomieChange} />
+          <input type="number" name="autonomie" value={autonomie} onChange={handleAutonomieChange} placeholder="km" />
           <label>Chargement</label>
-          <input type="number" name="chargement" value={tempsChargement} onChange={handleTempsChargementChange} />
+          <input type="number" name="chargement" value={tempsChargement} onChange={handleTempsChargementChange} placeholder="min." />
           <button type="button" onClick={sendRequest}>Calculer</button>
-        </form>
-        </div>
-        <div className="divtemps">
+            </form>
+          <div className="divtemps">
           <p>Temps de voyage : {temps} heures</p>
         </div>
+        </div>
+        <div className="map">
+              <img className="imgmap" src="https://as1.ftcdn.net/v2/jpg/02/25/77/30/1000_F_225773013_7VnI8Q20BuedFagxj2xvAcYNBTO5QhbN.jpg" alt="placeholder" />
+          </div>
+          </div>
+
       </div>
     </>
       
