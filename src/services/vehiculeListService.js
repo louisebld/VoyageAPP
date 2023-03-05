5a87265cbf2043d4170680c2e619265a77e8fb47
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-05 14:11:59 +0100

    graphql request

---
+++ src/services/vehiculeListService.js
@@ -0,0 +1,39 @@
+import { createClient, defaultExchanges } from '@urql/core';
+import qql from 'graphql-tag';
+
+export const headers = {
+  'x-client-id': '6402f92d85c5c3f0221ae397',
+  'x-app-id': '6402f92d85c5c3f0221ae399'
+};
+
+
+export const client = createClient({
+  url: 'https://api.chargetrip.io/graphql',
+  fetchOptions: {
+    method: 'POST',
+    headers,
+  },
+  exchanges: [...defaultExchanges],
+});
+
+export const vehicleListQuery = qql`
+query vehicleList($page: Int, $size: Int, $search: String) {
+  vehicleList(
+    page: $page, 
+    size: $size, 
+    search: $search, 
+  ) {
+    id
+    naming {
+      make
+      model
+      chargetrip_version
+    }
+    media {
+      image {
+        thumbnail_url
+      }
+    }
+  }
+}
+`;
