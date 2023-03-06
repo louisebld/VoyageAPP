5008bd9c5610e37a58329f1b82feca60c3418976
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-06 22:04:33 +0100

    vehicule list

---
+++ src/assets/vehiculelist.js
@@ -0,0 +1,175 @@
+export const vehiculeList = {
+  "vehicleList":
+    [
+    {
+      "id": "63ee3588a6b4b8c6cd870bc5",
+      "naming": {
+        "make": "BMW",
+        "model": "i3",
+        "chargetrip_version": "60 Ah REx",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63ef64b8793135e270141a12-bf21a9e8fbc5a3846fb05b4fa0859e0917b2202f.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "5f043b7cbc262f1627fc025d",
+      "naming": {
+        "make": "BMW",
+        "model": "i3",
+        "chargetrip_version": "94 Ah (2016-2017)",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/6250369c7d209b7fe2a5b71c-fe6b5beb4955d6b45c931a315b2481962a7fdab7.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "5d161beec9eef4074dd9d224",
+      "naming": {
+        "make": "BMW",
+        "model": "i3",
+        "chargetrip_version": "94 Ah (2017-2018)",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/625036f07d209b7fe2a5b7a9-f5fab1f38fa7e7db250f2a20b6d3e1e3dbc7d183.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "63ee33080eeaf3c693ed504f",
+      "naming": {
+        "make": "BMW",
+        "model": "i3",
+        "chargetrip_version": "94 Ah REx",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63ef646fa46d8de26bab1106-9b49bfae89d13f8980028f9281b6e8807c36d65f.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "63ee338e0f18fec69e4ed159",
+      "naming": {
+        "make": "BMW",
+        "model": "i3",
+        "chargetrip_version": "Range Extender",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63ef64873cf93de26e223d6c-2c44df6d20ebbf062b059c0b848ef2301a01866d.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "5f043c8cbc262f1627fc02bf",
+      "naming": {
+        "make": "BMW",
+        "model": "i3s",
+        "chargetrip_version": "120 Ah",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/5f731067efe67550e7895f09-8a062718d11cd37c947bcddf82338f03e348ac9e.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "5d161beec9eef4c250d9d225",
+      "naming": {
+        "make": "BMW",
+        "model": "i3s",
+        "chargetrip_version": "94 Ah (2017 - 2018)",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/625036e07d209b7fe2a5b792-0c1482b09160e397298ea4e157f923c979130270.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "63ee3393219ba9c69e14a1ff",
+      "naming": {
+        "make": "BMW",
+        "model": "i3s",
+        "chargetrip_version": "Range Extender",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63ef649ec6325ae27095a4a4-963eec8b7776ec82042ca650df11622c5fcc4f0b.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "637c113fe8ebbc93bd1124e3",
+      "naming": {
+        "make": "BMW",
+        "model": "i4",
+        "chargetrip_version": "eDrive35",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63c158127b2b063c8f81c4c3-fc5239edac8a527476c805594a2aa074853d7031.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    },
+    {
+      "id": "60b81c3494b0d06c63363aab",
+      "naming": {
+        "make": "BMW",
+        "model": "i4",
+        "chargetrip_version": "eDrive40",
+        "__typename": "VehicleListNaming"
+      },
+      "media": {
+        "image": {
+          "thumbnail_url": "https://cars.chargetrip.io/63c677f9f4ee435e90371cff-a1df9af73e259a684d7312755bbcb3f4d363fbe5.png",
+          "__typename": "VehicleImage"
+        },
+        "__typename": "VehicleListMedia"
+      },
+      "__typename": "VehicleList"
+    }
+  ]
+}
--- src/components/list.js
+++ src/components/list.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { createClient, defaultExchanges } from '@urql/core';
 import { vehicleListQuery } from '../services/vehiculeListService';
-
+import {vehiculeList} from '../assets/vehiculelist.js'
 const headers = {
   'x-client-id': '6402f92d85c5c3f0221ae397',
   'x-app-id': '6402f92d85c5c3f0221ae399'
@@ -22,11 +22,29 @@ function List() {
   var [data, setData] = React.useState(null);
 
   useEffect(() => {
+    setData(vehiculeList);
   // client.query(vehicleListQuery, { page: 10, size: 10, search: "" }).toPromise().then((result) => {
-  //   console.log(result.data);
+  //   // console.log(result.data);
   //   setData(result.data);
+  //   saveDataToJSON(result.data);
   // });
+    
   }, []);
+
+  
+  async function saveDataToJSON(dataa) {
+    try {
+      const blob = new Blob([JSON.stringify(dataa)], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'data.json';
+      a.click();
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
     return (
         <div>
